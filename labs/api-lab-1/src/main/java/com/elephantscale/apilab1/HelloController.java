55041e913d5225d38649d52e2d05c29deaadeeae
Mohammad Foolady <foolady@gmail.com>
2019-05-06 03:19:35 +0430

    Update lab

--- labs/api-lab-1/src/main/java/com/elephantscale/apilab1/HelloController.java
+++ labs/api-lab-1/src/main/java/com/elephantscale/apilab1/HelloController.java
@@ -8,10 +8,9 @@ import com.intel.analytics.zoo.pipeline.inference.JTensor;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @RestController
 public class HelloController {
-    
+
     @RequestMapping("/")
     public String index() {
         return "Recommendations Service\n";
@@ -19,28 +18,33 @@ public class HelloController {
 
     @RequestMapping("/recs")
     public String getRecs() {
-        String modelPath = System.getProperty("MODEL_PATH", "model.bin");
+        try {
+            String modelPath = System.getProperty("MODEL_PATH", "model.bin");
 
-        NueralCFJModel rcm = new NueralCFJModel();
+            NueralCFJModel rcm = new NueralCFJModel();
 
-        rcm.load(modelPath);
+            rcm.load(modelPath);
 
-        List<UserItemPair> userItemPairs = new ArrayList<>();
-        for(int i= 1; i < 10; i++){
-           userItemPairs.add(new UserItemPair(i, i+1));
-        }
-
-        List<List<JTensor>> jts = rcm.preProcess(userItemPairs);
-
-        List<List<JTensor>> finalResult = rcm.predict(jts);
-
-        String returnVal = "";
-
-        for(List<JTensor> fjt : finalResult){
-            for(JTensor t: fjt){
-                returnVal += t + "\n";
+            List<UserItemPair> userItemPairs = new ArrayList<>();
+            for (int i = 1; i < 10; i++) {
+                userItemPairs.add(new UserItemPair(i, i + 1));
             }
+
+            List<List<JTensor>> jts = rcm.preProcess(userItemPairs);
+
+            List<List<JTensor>> finalResult = rcm.predict(jts);
+
+            String returnVal = "";
+
+            for (List<JTensor> fjt : finalResult) {
+                for (JTensor t : fjt) {
+                    returnVal += t + "\n";
+                }
+            }
+            return returnVal;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
         }
-        return returnVal;
     }
 }
